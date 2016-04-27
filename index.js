31d9d6b75eda1a54d08b03e6bbf032c4cfa2dac6
Pyraxo <aaron.tua5@gmail.com>
2016-04-27 19:10:47 +0800

    base bot

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+require('babel-core/register')
+require('babel-polyfill')
+
+const Bot = require('./src').Bot
+let env = process.env
+
+let debug = {
+  'production': true,
+  'development': false
+}
+
+let bot = new Bot(!debug[env.NODE_ENV])
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const Bot = require('../lib/Bot')
+const BaseClient = require('../lib/base/BaseClient')
+const BaseCommand = require('../lib/base/BaseCommand')
+
+module.exports = {
+  Bot: Bot,
+  BaseClient: BaseClient,
+  BaseCommand: BaseCommand
+}
